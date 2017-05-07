7e203dfc7bc4d766744f96fd13b3e73cd07b642a
gichang-yang <ykcha9@gmail.com>
2017-05-07 22:08:58 +0900

    Initial commit

---
+++ app/src/main/java/com/td/yang_gichang/tododistributor/RegistTODO.java
@@ -0,0 +1,13 @@
+package com.td.yang_gichang.tododistributor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistTODO extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regist_todo);
+    }
+}
